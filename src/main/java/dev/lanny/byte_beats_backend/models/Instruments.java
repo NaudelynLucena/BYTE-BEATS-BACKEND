58e364b1472dbdd54a45c11a0d85bfb23fe57da8
JoseRD149 <joseromerodiaz89@gmail.com>
2025-02-21 15:01:07 +0100

    feat: add Piano model class

--- src/main/java/dev/lanny/byte_beats_backend/models/Instruments.java
+++ src/main/java/dev/lanny/byte_beats_backend/models/Instruments.java
@@ -1,5 +1,29 @@
 package dev.lanny.byte_beats_backend.models;
 
 public class Instruments {
+    private String name;
+    private int id;
+    private String type;
+    private String sound;
+
+    public Instruments(String name, int id, String type, String sound) {
+        this.name = name;
+        this.id = id;
+        this.type = type;
+        this.sound = sound;
+    }
+
+    public int getId() { return id;}
+    public  String getname() { return name;}
+    public String getType() { return type;}
+    public String getSound() { return sound;}
+
+    
+public void sound() { System.out.println(name + " sound" + " is playing" + sound); }
+
+       
+    
+ 
+
 
 }
