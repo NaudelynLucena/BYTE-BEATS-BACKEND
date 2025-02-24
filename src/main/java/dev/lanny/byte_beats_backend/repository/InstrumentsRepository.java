05056f032be1025691ab5af2c335b241c3a1e7c5
JoseRD149 <joseromerodiaz89@gmail.com>
2025-02-24 14:57:13 +0100

    feat: implemented InstrumentRepository class for data persistence

--- src/main/java/dev/lanny/byte_beats_backend/repository/InstrumentsRepository.java
+++ src/main/java/dev/lanny/byte_beats_backend/repository/InstrumentsRepository.java
@@ -1,5 +1,23 @@
-package dev.lanny.byte_beats_backend.server;
+package dev.lanny.byte_beats_backend.repository;
 
-public class WebServer {
+import java.util.List;
+
+import dev.lanny.byte_beats_backend.models.Instruments;
+import dev.lanny.byte_beats_backend.persistence.InstrumentDAO;
+
+public class InstrumentsRepository {
+    private final InstrumentDAO instrumentDAO;
+
+    public InstrumentsRepository(InstrumentDAO instrumentDAO) {
+        this.instrumentDAO = instrumentDAO;
+    }
+
+    public List<Instruments> getAllInstruments() {
+        return instrumentDAO.loadInstruments();
+    }
+
+    // public Instrument getInstrumentById(int id) {
+    // return instrumentDAO.findById(id);
+    // }
 
 }
