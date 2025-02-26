f7c91d29ce692b11d6c6a02befe8c7548e54e280
JoseRD149 <joseromerodiaz89@gmail.com>
2025-02-26 16:19:07 +0100

    feat: implement initial recording flow

---
+++ src/main/java/dev/lanny/byte_beats_backend/dtos/RecordingDto.java
@@ -0,0 +1,9 @@
+package dev.lanny.byte_beats_backend.dtos;
+
+public record RecordingDto(int id, String title, double duration) {
+    public  RecordingDto {
+
+     }
+
+
+}
--- src/main/java/dev/lanny/byte_beats_backend/models/Recording.java
+++ src/main/java/dev/lanny/byte_beats_backend/models/Recording.java
@@ -1,5 +1,47 @@
 package dev.lanny.byte_beats_backend.models;
 
 public class Recording {
+    private int id;
+    private String title;
+    private double duration;
+
+    public Recording(int id, String title, double duration) {
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void startRecording() {
+        System.out.println("Recording started");
+    }
+
+    public void stopRecording() {
+        System.out.println("Recording stopped");
+    }
+
+    public void playRecording() {
+        System.out.println("Recording playing");
+    }
+
+    @Override
+    public String toString() {
+        return "Recording{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
 
 }
