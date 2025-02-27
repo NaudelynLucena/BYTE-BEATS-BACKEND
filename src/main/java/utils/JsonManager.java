f750e5101dc87d85c978c9154020d733c8749366
LannyRivero <lannyrivero288@gmail.com>
2025-02-27 11:47:35 +0100

    add dependecy to pom, add method executeSQL to databaseConecction class and jsonToMap to JsonManager class

--- src/main/java/utils/JsonManager.java
+++ src/main/java/utils/JsonManager.java
@@ -1,9 +1,13 @@
 package utils;
 
+import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+
 
 public class JsonManager {
     private static final Gson gson = new Gson();
@@ -15,5 +19,15 @@ public class JsonManager {
     // convertir el Json a una lista de objetos
     public static <T>  List <T> fromJsonList(String json, Class<T> clazz) {
         return gson.fromJson(json, TypeToken.getParameterized(List.class, clazz).getType());
+    }
+    //Convierto Json a un objeto Java
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        return gson.fromJson(json, clazz);
+    }
+
+    //convierte JSon a Map
+    public static Map<String, Object> fromJsonToMap(String json) {
+        Type type = new TypeToken<Map<String, Object>>() {}.getType();
+        return gson.fromJson(json, type);
     }
 }
