e3fe6589a03308a6d38804295c2b9c5918d62982
JoseRD149 <joseromerodiaz89@gmail.com>
2025-02-28 16:43:52 +0100

    modified the and add getNotesByRecordingId

--- src/main/java/dev/lanny/byte_beats_backend/repository/RecordingRepository.java
+++ src/main/java/dev/lanny/byte_beats_backend/repository/RecordingRepository.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import dev.lanny.byte_beats_backend.dtos.NoteDto;
 import dev.lanny.byte_beats_backend.dtos.RecordingDto;
 import dev.lanny.byte_beats_backend.persistence.DatabaseConnection;
 
@@ -26,22 +27,22 @@ public class RecordingRepository {
     }
 
     public List<RecordingDto> getAllRecordings() {
-        List<RecordingDto> recording = new ArrayList<>();
+        List<RecordingDto> recordings = new ArrayList<>();
         String query = "SELECT * FROM recordings";
         try (Statement stmt = connection.createStatement();
-
-                ResultSet rc = stmt.executeQuery(query)) {
-            while (rc.next()) {
-                recording.add(new RecordingDto(
-                        rc.getInt("id"),
-                        rc.getString("title"),
-                        rc.getDouble("duration")));
+             ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
+                recordings.add(new RecordingDto(
+                        rs.getInt("id"),
+                        rs.getString("timestamp"),
+                        rs.getLong("duration"),
+                        rs.getString("instrument"),
+                        getNotesByRecordingId(rs.getInt("id"))));
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return recording;
+        return recordings;
     }
 
     public RecordingDto getRecordingById(int id) {
@@ -99,5 +100,23 @@ public class RecordingRepository {
             e.printStackTrace();
         }
         return false;
+    }
+    private List<NoteDto> getNotesByRecordingId(int recordingId) {
+        List<NoteDto> notes = new ArrayList<>();
+        String query = "SELECT midi, startTime, stopTime, duration FROM notes WHERE recordingId = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, recordingId);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                notes.add(new NoteDto(
+                        rs.getInt("midi"),
+                        rs.getLong("startTime"),
+                        rs.getLong("stopTime"),
+                        rs.getLong("duration")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return notes;
     }
 }
