d823ef74533d2956dfe7d62e76533acf7f38929b
NaudelynLucena <naulunau@gmail.com>
2025-02-28 16:12:47 +0100

    feat: added new NotaDto and updated RecordingDto

--- src/main/java/dev/lanny/byte_beats_backend/dtos/RecordingDto.java
+++ src/main/java/dev/lanny/byte_beats_backend/dtos/RecordingDto.java
@@ -1,9 +1,11 @@
 package dev.lanny.byte_beats_backend.dtos;
 
-public record RecordingDto(int id, String title, double duration) {
-    public  RecordingDto {
+import java.util.List;
 
-     }
-
-
-}
+public record RecordingDto(
+    int id,
+    String timestamp,
+    long duration,
+    String instrument,
+    List<NoteDto> notes
+) {}
